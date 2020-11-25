988ca5d8f5520d1ca2eb35b6de65f2505314ab0e
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-11-24 22:55:00 -0500

    Add cart controller

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -3,6 +3,7 @@ const app = express()
 const env = require('dotenv')
 // const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 
 // routes
@@ -10,6 +11,7 @@ const authRoutes = require('./routes/auth')
 const adminRoutes = require('./routes/admin/auth')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
+const carttRoutes = require('./routes/cart')
 
 // environment variables / constants
 env.config()
@@ -21,10 +23,13 @@ mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, { useNewUrlParser: true
 
 // app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.json())
+app.use('/public', express.static(path.join(__dirname, 'uploads')))
 
 app.use('/api', authRoutes) // 'api' must be in the URL to access it
 app.use('/api/', adminRoutes)
 app.use('/api/', categoryRoutes)
+app.use('/api/', productRoutes)
+app.use('/api/', cartRoutes)
 
 const PORT = process.env.PORT || 2000;
 
