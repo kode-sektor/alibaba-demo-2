07a8bb02d2db161db0857e692e50aa61323b9fcf
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-12-08 14:58:46 -0500

    Rearrange layout components

--- flipkart-clone/src/containers/HomePage/index.js
+++ flipkart-clone/src/containers/HomePage/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Header from '../../components/Header'
-// import Layout from '../../components/Layout'
-import MenuHeader from '../../components/MenuHeader'
+import Layout from '../../components/Layout'
 
 
 const HomePage = (props) => {
   return(
         <div>
-            <Header />
-            <MenuHeader />
+            <Layout>
+                Homepage
+            </Layout>
         </div>
     )
  }
