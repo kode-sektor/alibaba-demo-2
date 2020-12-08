89707c8d81a56dbb9db800f7f72275bb551f3356
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-12-07 20:18:11 -0500

    Remove initial stylesheet

--- flipkart-clone/src/App.js
+++ flipkart-clone/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./containers/HomePage";
 // import ProductListPage from "./containers/ProductListPage";
