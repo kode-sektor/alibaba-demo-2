9fca59b68a70bd136e99b2ebcf9733cb09403836
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-12-07 20:16:04 -0500

    Add homepage route

---
+++ flipkart-clone/src/containers/HomePage/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Header from '../../components/Header'
+// import Layout from '../../components/Layout'
+// import MenuHeader from '../../components/MenuHeader'
+
+/**
+* @author
+* @function HomePage
+**/
+
+const HomePage = (props) => {
+  return(
+        <div>
+            Home Page
+        </div>
+    )
+ }
+
+export default HomePage
