7f9211de616219ac8c512eed334a4d2774ec619a
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-12-07 20:58:58 -0500

    Add menu header

---
+++ flipkart-clone/src/components/MenuHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const index = () => {
+    return (
+        <div className="menuHeader">
+            MenuHeader
+        </div>
+    )
+}
+
+export default index
--- flipkart-clone/src/containers/HomePage/index.js
+++ flipkart-clone/src/containers/HomePage/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
-// import Header from '../../components/Header'
+import Header from '../../components/Header'
 // import Layout from '../../components/Layout'
-// import MenuHeader from '../../components/MenuHeader'
+import MenuHeader from '../../components/MenuHeader'
 
-/**
-* @author
-* @function HomePage
-**/
 
 const HomePage = (props) => {
   return(
         <div>
-            Home Page
+            <Header />
+            <MenuHeader />
         </div>
     )
  }
