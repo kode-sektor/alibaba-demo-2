1f4f51916eb25a53feed656354feb7efcec0f400
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-12-15 04:03:45 -0500

    Add 'admin page.js'

--- ecommerce-back-end/src/models/cart.js
+++ ecommerce-back-end/src/models/cart.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
     cartItems: [
         {
             product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
